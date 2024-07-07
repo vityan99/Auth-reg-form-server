b6dc26bdbc46fb386026a892d2253985027f401a
Виктор <vityan99@MacBook-Pro-Viktor.local>
2024-07-07 14:04:04 +0300

    Corrections

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const container = document.getElementById("container");
 signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
-``
+
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
